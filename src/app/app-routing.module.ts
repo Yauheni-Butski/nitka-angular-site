e0472a1796e3d095ba1fb7213e1ff410de99d2c7
YauheniButski <ybutski@gmail.com>
2018-10-31 10:04:35 +0300

    Preparing for developing. Just initial clearing actions.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+/* const routes: Routes = [
+  {
+    path: '/gallery', component: GalleryComponent
+  }
+] */
+
+@NgModule({
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
