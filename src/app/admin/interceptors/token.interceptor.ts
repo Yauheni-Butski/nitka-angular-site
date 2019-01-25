2cb1d8e0641f70471b975fd36d815e2def1d062d
YauheniButski <ybutski@gmail.com>
2019-01-25 17:45:03 +0300

    Add handling 401 error for Admin module.

--- src/app/admin/interceptors/token.interceptor.ts
+++ src/app/admin/interceptors/token.interceptor.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { AuthService }      from '../../auth/auth.service';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
+  constructor(public authService: AuthService, public router: Router) {}
+
   intercept(request: HttpRequest<any>,next: HttpHandler): Observable<HttpEvent<any>> {
+      var httpHandle: Observable<HttpEvent<any>>;
+      var isUserTokenExist: boolean = false;
+
       const loggedInData = localStorage.getItem("loggedIn");
       if (loggedInData){
         var isLoggedIn: boolean = JSON.parse(localStorage.getItem('loggedIn'));
@@ -16,11 +25,40 @@ export class TokenInterceptor implements HttpInterceptor {
                     headers: request.headers.set("Authorization", `Bearer ${userToken}`)
                 });
 
-                return next.handle(clonedHeader);
+                //return next.handle(clonedHeader);
+                httpHandle = next.handle(clonedHeader);
+                isUserTokenExist = true;
             }
         }
       }
 
-      return next.handle(request);
+      //return next.handle(request);
+      if (!isUserTokenExist){
+        httpHandle = next.handle(request);
+      }
+
+      return httpHandle
+        .pipe(
+            tap(
+                (event: HttpEvent<any>) => {},
+                (err: any) => {
+                    if (err instanceof HttpErrorResponse) {
+                        if (err.status === 401) {
+                            //1. Logout
+                            this.authService.logout();
+                            //2. Redirect to login
+                            // Set our navigation extras object
+                            // that passes on our global query params and fragment
+                            let navigationExtras: NavigationExtras = {
+                                queryParamsHandling: 'preserve',
+                                preserveFragment: true
+                            };
+                            // Redirect the user
+                            this.router.navigate(['/login'], navigationExtras);
+                        }
+                    }
+                }
+            )
+        );
   }
 }
