32c06ce05f2a42e05440647b339f860d53d4631a
YauheniButski <ybutski@gmail.com>
2019-06-26 13:46:48 +0300

    minor fix

--- src/app/components/_shared/social-icons/social-icons.component.ts
+++ src/app/components/_shared/social-icons/social-icons.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { SocialLink } from '../../models/socialLink';
+import { SocialLink } from '../../../models/socialLink';
 
 @Component({
   selector: 'app-social-icons',
